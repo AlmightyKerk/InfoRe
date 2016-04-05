89f45338888b726bb334e01f7cbe5b50992dfccd
Jayden Kerk <kerk90@Hotmail.com>
2016-04-05 21:29:22 +0800

    first commit

---
+++ src/SolrJ.java
@@ -0,0 +1,27 @@
+import java.net.MalformedURLException;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+
+
+public class SolrJ {
+	public static void main(String[] args) throws MalformedURLException, SolrServerException {
+	    HttpSolrServer solr = new HttpSolrServer("http://localhost:8983/solr");
+
+	    SolrQuery query = new SolrQuery();
+	    query.setQuery("sony digital camera");
+	    query.addFilterQuery("cat:electronics","store:amazon.com");
+	    query.setFields("id","price","merchant","cat","store");
+	    query.setStart(0);    
+	    query.set("defType", "edismax");
+
+	    QueryResponse response = solr.query(query);
+	    SolrDocumentList results = response.getResults();
+	    for (int i = 0; i < results.size(); ++i) {
+	      System.out.println(results.get(i));
+	    }
+	  }
+}
